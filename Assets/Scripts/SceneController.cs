98c4366721730a14f674bcce238a518a49196dac
Sergio Sánchez <serjsa385@gmail.com>
2022-05-16 13:11:43 +0200

    Spectator Camera working nicely
    HMD and Monitor show different images. Ready to go

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -7,6 +7,9 @@ public class SceneController : MonoBehaviour
     public static SceneController instance;  // Singleton
 
     public UISpectatorController spectatorCanvas;
+
+    public Camera cameraHMD;
+    public Camera cameraSpectator;
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +18,10 @@ public class SceneController : MonoBehaviour
         instance = this;
 
         spectatorCanvas.SetSpectatorText("Welcome, spectator.");
+
+        // Change the camera depths so the spectator camera show on pc monitor and HMD on HMD
+        cameraHMD.depth = 0;
+        cameraSpectator.depth = 1;
     }
     
 
